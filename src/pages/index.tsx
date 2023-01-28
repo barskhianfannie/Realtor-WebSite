ffbf7ef3c4e5b6e1b5e5cf99c0249c26549da7c5
Fannie Barskhian <barskhianfannie@gmail.com>
2023-01-28 14:07:39 -0800

    fix: start

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,13 @@
 
 import Landing from "../components/Landing";
-import Services from "../components/Services";
-import { ScheduleCard } from "../components/Schedule";
-import type { NextPage } from 'next';
 
 
 function HomePage (){
 
   return (
-    <div>
+    <div className="bg-white">
       <div className="relative mx-auto  w-5/6 lg:max-w-7xl">
       <Landing  frontPage={true}/>
-      <Services/>
-      <ScheduleCard/>
       </div>
     </div>
   );
